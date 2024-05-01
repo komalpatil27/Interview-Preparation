6be6fdf39dc8aebbfd8cc396a9af3a1483f78f40
Komal <komalsubhash2797@gmail.com>
2024-05-01 22:19:04 +0530

    Error Handling

--- nodePractice/middleware/errorHandler.js
+++ nodePractice/middleware/errorHandler.js
@@ -1,10 +1,30 @@
 const errorhandler = (err, req, res, next) => {
     console.log(err, req, res , 'response')
     const statusCode = res.statusCode ? res.statusCode : 500
-    res.status(statusCode).json({
+    let error  ={
         mesage: err.message,
         stackTrace: err.stack
-    })
+    }
+    switch(statusCode){
+        case 400:
+            res.status(400).json({
+                title : 'Validation failed',
+                ...error
+            })
+            break;
+        case 500:
+            res.status(500).json({
+                title : 'Internal error occured',
+                ...error
+            })
+            break;
+        default:
+            res.status(404).json({
+                title : 'Not Found',
+                ...error
+            })
+            break;
+    }
 }
 
 export default errorhandler
